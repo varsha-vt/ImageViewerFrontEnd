37f3dd01917692ce33e4444e23173abc45c0f192
varshat <varshat91@gmail.com>
2021-05-29 15:28:11 +0530

    Adding profile page structure

---
+++ src/screens/profile/Profile.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Profile.css";
+import Header from "../../common/Header.js"
+
+class Profile extends Component {
+    render(){
+        return(
+            <div>
+                <Header></Header>
+                Profile Page
+            </div>
+        )
+    }
+}
+
+export default Profile;
